3c362ea3b74aad056c765cf38ab0df258de1ab69
h-huynh <henryh1404@gmail.com>
2023-08-05 21:58:09 -0500

    Update AddressTable.tsx

--- tax-prep-system-frontend/src/components/Address/AddressTable.tsx
+++ tax-prep-system-frontend/src/components/Address/AddressTable.tsx
@@ -1,6 +1,6 @@
-import { useTranslation } from 'react-i18next';
+//import { useTranslation } from 'react-i18next';
 const AddressTable = (): React.ReactElement => {
-    const { t } = useTranslation();
+    //const { t } = useTranslation();
 
     
     return(
