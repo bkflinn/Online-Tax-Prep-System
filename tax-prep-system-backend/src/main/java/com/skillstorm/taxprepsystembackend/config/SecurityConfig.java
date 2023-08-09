eb21b4651a04c7bcecf00d2a2531048bcac2df0f
Flinn, Brandon Kenneth <bkflinn@iu.edu>
2023-08-09 17:33:34 -0400

    oauth2 not working

---
+++ tax-prep-system-backend/src/main/java/com/skillstorm/taxprepsystembackend/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.skillstorm.taxprepsystembackend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests(authorizeRequests -> {
+            authorizeRequests.anyRequest().authenticated();
+        })
+        .csrf(csrf -> csrf.disable())
+        .oauth2Login();
+
+        return http.build();
+    }
+    
+}
